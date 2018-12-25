7334988e001aef273098973d954fff7160208003
Rajat Pawar <rajat.pawar@RajatPawar.local>
2018-12-25 17:54:02 +0530

    Added bitcount using x & (x-1) to delete right most bit trick

---
+++ playground/bitcount.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int bitcount(unsigned x) {
+    int set = 0;
+
+    // Until the shift or AND results a 0
+    while(x) {
+        // Deletes right most set bit (increment and shift to right)
+        x = (x & (x-1));
+        x >>= 1;
+        set++;
+    }
+    return set;
+}
+
+int main() {
+    unsigned x = 255;
+    printf("Set bits are: %d", bitcount(x));
+    return 0;
+}
