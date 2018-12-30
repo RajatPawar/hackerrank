816bdccd9d285c062ca843892a5a576c6a1d774a
Rajat Pawar <rajat.pawar@RajatPawar.local>
2018-12-31 00:17:32 +0530

    Reverse recursion

---
+++ playground/reverse-string-recurse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void reverse(char *arr, int from, int to) {
+    
+    if(from > to) return;
+    
+    char temp = arr[from];
+    arr[from] = arr[to];
+    arr[to] = temp;   
+
+    reverse(arr, from + 1, to - 1); 
+}
+
+int main() {
+    char arr[] = "This";
+    reverse(arr, 0, sizeof(arr) - 2); // Subtracting delimiter and 1th index
+    printf("\n%s\n", arr, sizeof(test), sizeof(a));
+    return 0;
+}
