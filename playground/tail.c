693dfdf30e8d99040efdf383cd6a8309f9c3513f
Rajat Pawar <rajat.pawar@RajatPawar.local>
2019-01-06 17:47:06 +0530

    Added all cases for tail

--- playground/tail.c
+++ playground/tail.c
@@ -4,10 +4,23 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    
+    int numstrings = argc - 1, 
+        linestoprint = 10;
+
+    // numstrings counts how many strings are there, lines are default 10
+        
     while(--argc > 0 && *(++argv)[0] == '-') {
         for(int i = 1; i < strlen(*argv); i++) (*argv)[i - 1] = (*argv)[i];
         (*argv)[strlen(*argv) - 1] = '\0';
-        int lines = atoi(*argv);
+        linestoprint = atoi(*argv);
+        numstrings--; // counting this param, we have one less string 
     }
+    
+    while(numstrings && argc-- > 0) {
+        if(numstrings-- - linestoprint <= 0) 
+            printf("%s\n", *(argv++));
+    }
+
     return 0;
 }
